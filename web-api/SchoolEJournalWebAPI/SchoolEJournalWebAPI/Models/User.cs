5ad55e672df0e51e18e299ab6db555b981c523a9
DocentSzachista <57325417+DocentSzachista@users.noreply.github.com>
2022-01-02 19:19:00 +0100

    init commit with db connection configured

---
+++ web-api/SchoolEJournalWebAPI/SchoolEJournalWebAPI/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolEJournalWebAPI.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int UserType{ get; set; }
+        public int ParentId { get; set; }
+        public int ClassId { get; set; }
+
+    }
+}
