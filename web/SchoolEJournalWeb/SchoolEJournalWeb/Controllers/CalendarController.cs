13d790ff9c91af082fa0fcbc9bdcd8cefb22881f
kdt111 <j.malek@onet.eu>
2022-01-07 16:39:50 +0100

    Calendar finished

--- web/SchoolEJournalWeb/SchoolEJournalWeb/Controllers/CalendarController.cs
+++ web/SchoolEJournalWeb/SchoolEJournalWeb/Controllers/CalendarController.cs
@@ -3,11 +3,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SchoolEJournalWeb.Models;
 
 namespace SchoolEJournalWeb.Controllers
 {
 	public class CalendarController : Controller
 	{
+		private readonly SchoolEJournalContext _context;
+
+		public CalendarController(SchoolEJournalContext context)
+		{
+			_context = context;
+		}
+
 		public IActionResult Index()
 		{
 			return View();
@@ -15,12 +23,36 @@ namespace SchoolEJournalWeb.Controllers
 
 		public IActionResult ShowCalendar()
 		{
+			return ChangeDate(DateTime.Now.ToString("yyyy-MM-dd"));
+		}
+
+		[HttpPost]
+		public IActionResult ChangeDate(string selectedDate)
+		{
+			DateTime date = DateTime.Parse(selectedDate);
+
+			ViewData["DisplayedDate"] = date.ToString("yyyy-MM-dd");
+			ViewData["CalendarData"] = CreateCalendar(date);
 			return View("~/Views/User/SharedResources/CalendarView.cshtml");
 		}
 
-		public IActionResult ChangeDate()
+		public List<string> CreateCalendar(DateTime date)
 		{
-			return View();
+			int userId = int.Parse(HttpContext.User.Claims.ToList()[0].Value);
+			List<string> returnList = new List<string>();
+
+			var query =
+				from l in _context.DisplayLessons
+				join u in _context.Users
+				on l.ClassName equals u.Class.ClassName
+				where l.StartTime.Date == date.Date
+				orderby l.StartTime.TimeOfDay ascending
+				select $"{l.SubjectName} ({l.StartTime.ToShortTimeString()} - {l.EndTime.ToShortTimeString()}) Temat: {(string.IsNullOrEmpty(l.Topic) ? "Brak tematu" : l.Topic)}";
+
+			foreach (string lessonText in query)
+				returnList.Add(lessonText);
+
+			return returnList;
 		}
 	}
 }
